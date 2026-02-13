166a194e04432242a47c3840eff192c5c4f5eb2f
DanBee <eksql8061@naver.com>
2026-02-13 15:01:52 +0900

    onetomany 단방향 조회 테스트

--- jpa-advance/src/main/java/com/sparta/jpaadvance/entity/User.java
+++ jpa-advance/src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -17,11 +17,11 @@ public class User {
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy = "user")
-    private List<Food> foodList = new ArrayList<>();
-
-    public void addFoodList(Food food) {
-        this.foodList.add(food);
-        food.setUser(this);
-    }
+//    @OneToMany(mappedBy = "user")
+//    private List<Food> foodList = new ArrayList<>();
+//
+//    public void addFoodList(Food food) {
+//        this.foodList.add(food);
+//        food.setUser(this);
+//    }
 }
---
+++ jpa-advance/src/test/java/com/sparta/jpaadvance/relation/OneToManyTest.java
@@ -0,0 +1,63 @@
+package com.sparta.jpaadvance.relation;
+
+import com.sparta.jpaadvance.entity.Food;
+import com.sparta.jpaadvance.entity.User;
+import com.sparta.jpaadvance.repository.FoodRepository;
+import com.sparta.jpaadvance.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+@Transactional
+@SpringBootTest
+public class OneToManyTest {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    FoodRepository foodRepository;
+
+    @Test
+    @Rollback(value = false)
+    @DisplayName("1대n 방향테스트")
+    void test1() {
+        User user = new User();
+        user.setName("Robbie");
+
+        User user2 = new User();
+        user2.setName("Robbert");
+
+        Food food = new Food();
+        food.setName("츼킨");
+        food.setPrice(10000);
+        food.getUserList().add(user);
+        food.getUserList().add(user2);
+
+        userRepository.save(user);
+        userRepository.save(user2);
+        foodRepository.save(food);
+    }
+
+    @Test
+    @DisplayName("1대N 조회 테스트")
+    void test2() {
+        Food food = foodRepository.findById(2L).orElseThrow(NullPointerException::new);
+        System.out.println("food.getName() = " + food.getName());
+
+        // 해당 음식을 주문한 고객 정보 조회
+        List<User> userList = food.getUserList();
+        for (User user : userList) {
+            System.out.println("user.getName() = " + user.getName());
+        }
+    }
+
+
+
+
+
+
+}

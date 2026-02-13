e8a2256d2b2fbf162a6469b0c995b279c1f85bc6
DanBee <eksql8061@naver.com>
2026-02-13 15:15:37 +0900

    Manytomany 단방향 테스트

--- jpa-advance/src/main/java/com/sparta/jpaadvance/entity/Food.java
+++ jpa-advance/src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -23,7 +23,15 @@ public class Food {
     //    @JoinColumn(name = "user_id")
     //    private User user;
 
-    @OneToMany
-    @JoinColumn(name = "food_id") // users 테이블에 food_id 컬럼
+    //    @OneToMany
+    //    @JoinColumn(name = "food_id") // users 테이블에 food_id 컬럼
+    //    private List<User> userList = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "orders",
+            joinColumns = @JoinColumn(name = "food_id"), //order테이블로 조인할 컬럼 설정
+            inverseJoinColumns = @JoinColumn(name = "user_id") // 반대 위치인 user Entity에서 중간으로 조인할 컬럼
+    ) // 중간테이블생성
+
     private List<User> userList = new ArrayList<>();
 }

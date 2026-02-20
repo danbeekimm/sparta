ace92c3d62cae7b8c8894da7f87d32e71d8c5804
DanBee <eksql8061@naver.com>
2026-02-20 12:11:08 +0900

    통합테스트

--- myselectshop/src/main/java/com/sparta/myselectshop/repository/ProductFolderRepository.java
+++ myselectshop/src/main/java/com/sparta/myselectshop/repository/ProductFolderRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface productFolderRepository extends JpaRepository<ProductFolder, Long> {
+public interface ProductFolderRepository extends JpaRepository<ProductFolder, Long> {
     Optional<ProductFolder> findByProductAndFolder (Product product, Folder folder);
 }

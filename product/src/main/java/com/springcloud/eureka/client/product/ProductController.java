a639a73bac468f60b878aaed926b26eedabd63d2
danbee <danbee@gmail.com>
2026-02-23 21:52:51 +0900

    msa config test

--- product/src/main/java/com/springcloud/eureka/client/product/ProductController.java
+++ product/src/main/java/com/springcloud/eureka/client/product/ProductController.java
@@ -1,18 +1,22 @@
 package com.springcloud.eureka.client.product;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class ProductController {
-
-    @Value("${server.port}")
+    @Value("${server.port}") // 애플리케이션이 실행 중인 포트를 주입받습니다.
     private String serverPort;
 
-    @GetMapping("/product")
-    public String product() {
-        return "product info!!!! from port : "+ serverPort;
+    @Value("${message}")
+    private String message;
+
+    @GetMapping
+    public String getProduct() {
+        return "Product detail from PORT : " + serverPort + " and message : " + this.message ;
     }
 }
